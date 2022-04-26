4b79a7b35b39dfcc1c395b1718f5dd23340aac67
unknown <sli@nextpathway.com>
2022-04-26 13:15:30 -0400

    update test cases

--- src/test/java/com/comcast/lcs/webservice/validator/LcsValidatorTest.java
+++ src/test/java/com/comcast/lcs/webservice/validator/LcsValidatorTest.java
@@ -1,7 +1,54 @@
 package com.comcast.lcs.webservice.validator;
 
+import com.comcast.lcs.webservice.exceptions.BadRequestException;
+import com.comcast.lcs.webservice.exceptions.InputBodyNotFoundException;
+import com.comcast.lcs.webservice.exceptions.StringsNotFoundOrEmptyException;
+import com.comcast.lcs.webservice.model.InputBody;
+import com.comcast.lcs.webservice.model.InputString;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith(MockitoExtension.class)
+public class LcsValidatorTest {
+    LcsValidator lcsValidator;
+    InputBody inputBody;
 
-class LcsValidatorTest {
+    @BeforeEach
+    void  setUp(){
+        lcsValidator = new LcsValidator();
+    }
 
+    @Test
+    void validateNullInputBody(){
+        assertThrows(InputBodyNotFoundException.class, () -> lcsValidator.validateInputBody(inputBody));
+    }
+
+    @Test
+    void validateEmptyInputStrings(){
+        inputBody = new InputBody();
+        List<InputString> inputStrings = new ArrayList<>();
+        inputBody.setSetOfStrings(inputStrings);
+        assertThrows(StringsNotFoundOrEmptyException.class, () -> lcsValidator.validateInputBody(inputBody));
+    }
+
+    @Test
+    void validateDuplicateInputStrings(){
+        inputBody = new InputBody();
+        List<InputString> inputStrings = new ArrayList<>();
+        inputStrings.add(InputString.builder()
+                .value("abcd")
+                .build());
+
+        inputStrings.add(InputString.builder()
+                .value("abcd")
+                .build());
+        inputBody.setSetOfStrings(inputStrings);
+        assertThrows(BadRequestException.class, () -> lcsValidator.validateInputBody(inputBody));
+    }
 }
