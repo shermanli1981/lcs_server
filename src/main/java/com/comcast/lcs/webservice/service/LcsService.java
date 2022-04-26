3be5acd0b019683fc0b254c4fec99de827096f47
unknown <sli@nextpathway.com>
2022-04-26 12:34:31 -0400

    update logic

--- src/main/java/com/comcast/lcs/webservice/service/LcsService.java
+++ src/main/java/com/comcast/lcs/webservice/service/LcsService.java
@@ -43,51 +43,5 @@ public class LcsService implements IService{
         Collections.sort(res, Comparator.comparing(OutputString::getValue));
         return res;
     }
-
-//    public static List<OutputString> findLongestSubString(String arr[])
-//    {
-//        // Determine size of the array
-//        int n = arr.length;
-//
-//        // Take first word from array as reference
-//        String s = arr[0];
-//        int len = s.length();
-//        int maxLen = 0;
-//        String res = "";
-//        List<String> r = new ArrayList<>();
-//        for (int i = 0; i < len; i++) {
-//            for (int j = i + 1; j <= len; j++) {
-//
-//                // generating all possible substrings
-//                // of our reference string arr[0] i.e s
-//                String stem = s.substring(i, j);
-//                int k = 1;
-//                for (k = 1; k < n; k++)
-//
-//                    // Check if the generated stem is
-//                    // common to all words
-//                    if (!arr[k].contains(stem))
-//                        break;
-//
-//                // If current substring is present in
-//                // all strings and its length is greater
-//                // than current result
-//                if (k == n && res.length() <= stem.length()){
-//                    res = stem;
-//                    r.add(stem);
-//                    maxLen = res.length();
-//                }
-//            }
-//        }
-//
-//        List<OutputString> endResult = new ArrayList<>();
-//        Collections.sort(r); //If there is more than one LCS return them all in alphabetic order.
-//        for(int i = 0; i < r.size(); i++){
-//            if(r.get(i).length() == maxLen){
-//                endResult.add(new OutputString(r.get(i)));
-//            }
-//        }
-//
-//        return endResult;
-//    }
+    
 }
